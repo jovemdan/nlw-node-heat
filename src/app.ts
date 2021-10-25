8ad3707531e5ef185810f777d70990b0a2182cca
jovemdan <danilogdev@gmail.com>
2021-10-25 15:10:01 -0300

    feat: Create MessageContoller, MessageService, Migrations

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,40 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
 
-import { router } from './routes';
+import { Server } from "socket.io";
+
+import { router } from "./routes";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
 app.use(express.json());
 
 app.use(router);
 
-app.get('/github', (request, response) => {
+app.get("/github", (request, response) => {
   response.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
   );
 });
 
-app.get('/signin/callback', (request, response) => {
+app.get("/signin/callback", (request, response) => {
   const { code } = request.query;
   return response.json(code);
 });
 
-app.listen(4000, () => console.log('Server is running on port 4000'));
+export { serverHttp, io };
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,18 +1,20 @@
-import axios from "axios"
+import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken";
 
 interface IAccesTokenResponse {
-  access_token: string
+  access_token: string;
 }
 interface IUserResponse {
-  avatar_url: string
-  login: string
-  id: number
-  name: string
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
 }
 
 class AuthenticateUserService {
   async execute(code: string) {
-    const url = "https://github.com/login/oauth/access_token"
+    const url = "https://github.com/login/oauth/access_token";
 
     const { data: accesTokenResponse } = await axios.post<IAccesTokenResponse>(
       url,
@@ -27,7 +29,7 @@ class AuthenticateUserService {
           Accept: "application/json",
         },
       }
-    )
+    );
     const response = await axios.get<IUserResponse>(
       "https://api.github.com/user",
       {
@@ -35,10 +37,44 @@ class AuthenticateUserService {
           authorization: `Bearer ${accesTokenResponse.access_token}`,
         },
       }
-    )
+    );
 
-    return response.data
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { token, user };
   }
 }
 
-export { AuthenticateUserService }
+export { AuthenticateUserService };
